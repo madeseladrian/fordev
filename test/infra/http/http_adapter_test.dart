3ffc4532637e7b1db2d665b4e18299841355ee16
mades eladrian <mades.eladrian@gmail.com>
2022-05-18 11:44:22 -0300

    move HttpAdapter to its own file

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,62 +1,12 @@
-import 'dart:convert';
-
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
 import 'package:fordev/data/http/http.dart';
+import 'package:fordev/infra/http/http.dart';
 
 class ClientSpy extends Mock implements Client {}
-
-class HttpAdapter  {
-  final Client client;
-
-  HttpAdapter({required this.client});
-
-  Future<Map?> request({
-    required String url,
-    required String method,
-    Map? body
-  }) async {
-    final headers = {
-      'Content-Type': 'application/json',
-      'accept': 'application/json'
-    };
-    var response = Response('', 500);
-    try {
-      if (method == 'post') {
-        response = await client.post(
-          Uri.parse(url),
-          headers: headers,
-          body: body
-        );
-      }
-    } catch (error) {
-      throw HttpError.serverError;
-    }
-    
-    return _handleResponse(response);
-  }
-
-  Map? _handleResponse(Response response) {
-    if (response.statusCode == 200) {
-      return response.body.isEmpty ? null : jsonDecode(response.body);
-    } else if (response.statusCode == 204) {
-      return null;
-    } else if (response.statusCode == 400) {
-      throw HttpError.badRequest;
-    } else if (response.statusCode == 401) {
-      throw HttpError.unauthorized;
-    } else if (response.statusCode == 403) {
-      throw HttpError.forbidden;
-    } else if (response.statusCode == 404) {
-      throw HttpError.notFound;
-    } else {
-      throw HttpError.serverError;
-    } 
-  }
-}
 
 void main() {
   late HttpAdapter sut;
