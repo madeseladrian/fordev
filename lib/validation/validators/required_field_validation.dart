87ca3d95b00b7e06caa8f84c7ac7cf3842368d8c
mades eladrian <mades.eladrian@gmail.com>
2022-05-21 19:44:17 -0300

    implement FieldValidation in RequiredFieldValidation

--- lib/validation/validators/required_field_validation.dart
+++ lib/validation/validators/required_field_validation.dart
@@ -1,10 +1,13 @@
 import '../../presentation/helpers/helpers.dart';
+import '../protocols/protocols.dart';
 
-class RequiredFieldValidation {
+class RequiredFieldValidation implements FieldValidation {
+  @override
   final String field;
  
   const RequiredFieldValidation(this.field);
  
+  @override
   ValidationError? validate(String? value) {
     return value?.isNotEmpty == true ? null : ValidationError.requiredField;
   }
