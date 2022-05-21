1f92b56a4a24f07a4b5a935ce247e5749ec5ade0
mades eladrian <mades.eladrian@gmail.com>
2022-05-21 19:46:09 -0300

    extend Equatable in RequiredFieldValidation

--- lib/validation/validators/required_field_validation.dart
+++ lib/validation/validators/required_field_validation.dart
@@ -1,10 +1,15 @@
+import 'package:equatable/equatable.dart';
+
 import '../../presentation/helpers/helpers.dart';
 import '../protocols/protocols.dart';
 
-class RequiredFieldValidation implements FieldValidation {
+class RequiredFieldValidation extends Equatable implements FieldValidation {
   @override
   final String field;
  
+  @override
+  List get props => [field];
+
   const RequiredFieldValidation(this.field);
  
   @override
