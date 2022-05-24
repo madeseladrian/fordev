b126e2c46c5c169e197634063f9337ea47406f64
mades eladrian <mades.eladrian@gmail.com>
2022-05-24 18:22:46 -0300

    ensure MinLengthValidation returns error if value is empty

---
+++ test/validation/validators/min_length_validation_test.dart
@@ -0,0 +1,28 @@
+import 'package:test/test.dart';
+
+import 'package:fordev/presentation/helpers/helpers.dart';
+import 'package:fordev/validation/protocols/field_validation.dart';
+
+class MinLengthValidation implements FieldValidation {
+  final String field;
+  final int size;
+
+  MinLengthValidation({ required this.field, required this.size});
+
+  @override
+  ValidationError? validate(String value) {
+    return ValidationError.invalidField;
+  }
+}
+
+void main() {
+  late MinLengthValidation sut;
+
+  setUp(() {
+    sut = MinLengthValidation(field: 'any_field', size: 3);
+  });
+
+  test('1 - Should return error if value is empty', () {
+    expect(sut.validate(''), ValidationError.invalidField);
+  });
+}
