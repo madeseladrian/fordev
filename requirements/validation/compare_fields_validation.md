08388ab51b019a615320a08ae197ebf952b6f01c
mades eladrian <mades.eladrian@gmail.com>
2022-05-24 19:02:46 -0300

    ensure CompareFieldsValidation return null if values are equal

--- requirements/validation/compare_fields_validation.md
+++ requirements/validation/compare_fields_validation.md
@@ -2,3 +2,4 @@
 
 > ## Validações
 1. ✅ Deve retornar error se o valor não é igual.
+2. ✅ Deve retornar null se o valor é igual.
--- test/validation/validators/compare_fields_validation_test.dart
+++ test/validation/validators/compare_fields_validation_test.dart
@@ -7,11 +7,16 @@ void main() {
   late CompareFieldsValidation sut;
 
   setUp(() {
-    sut = CompareFieldsValidation(field: 'any_field', valueToCompare: 'other_field');
+    sut = CompareFieldsValidation(field: 'any_field', valueToCompare: 'any_value');
   });
 
-  test('Should return error if value is not equal', () {
+  test('1 - Should return error if values are not equal', () {
     final error = sut.validate('wrong_value');
     expect(error, ValidationError.invalidField);
+  });
+
+  test('2 - Should return error if values are not equal', () {
+    final error = sut.validate('any_value');
+    expect(error, null);
   });
 }
