e29d9a729b9b1347a736f992a5135e5e44af5f87
mades eladrian <mades.eladrian@gmail.com>
2022-05-24 13:31:07 -0300

    ensure GetxSignUpPresenter returns null if validation email succeds

--- test/presentation/presenters/getx_signup_presenter_test.dart
+++ test/presentation/presenters/getx_signup_presenter_test.dart
@@ -71,6 +71,14 @@ void main() {
    
     sut.emailErrorStream.listen(expectAsync1((error) => expect(error, UIError.invalidField)));
     sut.isFormValidStream.listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    sut.validateEmail(email);
+    sut.validateEmail(email);
+  });
+
+  test('10 - Should returns null if validation email succeeds', () async {
+    sut.emailErrorStream.listen(expectAsync1((error) => expect(error, null)));
+    sut.isFormValidStream.listen(expectAsync1((isValid) => expect(isValid, false)));
     
     sut.validateEmail(email);
     sut.validateEmail(email);
