3fb47070865e3d91d126c67efde3e68d1def5007
mades eladrian <mades.eladrian@gmail.com>
2022-06-02 17:32:39 -0300

    move SurveyResult to its own file'

--- lib/ui/pages/survey_result/survey_result_page.dart
+++ lib/ui/pages/survey_result/survey_result_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../components/components.dart';
 import '../../helpers/helpers.dart';
+import 'components/components.dart';
 import 'survey_result.dart';
 
 class SurveyResultPage extends StatelessWidget {
@@ -34,62 +35,7 @@ class SurveyResultPage extends StatelessWidget {
                 );
               }
               if (snapshot.hasData) {
-                return ListView.builder(
-                  itemBuilder: (context, index) {
-                    if (index == 0) {
-                      return Container(
-                        padding: const EdgeInsets.fromLTRB(20, 40, 20, 20),
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).disabledColor.withAlpha(90)
-                        ),
-                        child: const Text('Qual é seu framework web favorito?'),
-                      );
-                    }
-                    return Column(
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.all(15),
-                          decoration: BoxDecoration(
-                            color: Theme.of(context).backgroundColor
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Image.network(
-                                'http://fordevs.herokuapp.com/static/img/logo-angular.png',
-                                width: 40,
-                              ),
-                              const Expanded(
-                                child: Padding(
-                                  padding: EdgeInsets.symmetric(horizontal: 10),
-                                  child: Text(
-                                    'Angular', style: TextStyle(fontSize: 16)
-                                  ),
-                                )),
-                              Text(
-                                '100%', 
-                                style: TextStyle(
-                                  fontSize: 16, 
-                                  fontWeight: FontWeight.bold,
-                                  color: Theme.of(context).primaryColor
-                                )
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 10),
-                                child: Icon(
-                                  Icons.check_circle,
-                                  color: Theme.of(context).highlightColor,
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                        const Divider(height: 1)
-                      ],
-                    );
-                  },
-                  itemCount: 4,
-                );
+                return const SurveyResult();
               }
               return const SizedBox();
             }
