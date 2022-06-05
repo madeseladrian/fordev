6a04c5f85610e63624608d4b0d02314d36da2b28
mades eladrian <mades.eladrian@gmail.com>
2022-06-05 10:37:50 -0300

    create makeRemoteLoadSurveyResultWithLocalFallback factory

--- lib/main/factories/usecases/load_survey_result_factory.dart
+++ lib/main/factories/usecases/load_survey_result_factory.dart
@@ -1,9 +1,24 @@
 import '../../../data/usecases/usecases.dart';
+import '../../../domain/usecases/usecases.dart';
+import '../../composites/composites.dart';
 import '../factories.dart';
 
 RemoteLoadSurveyResult makeRemoteLoadSurveyResult(String surveyId) {
   return RemoteLoadSurveyResult(
     httpClient: makeAuthorizeHttpClientDecorator(),
     url: makeApiUrl('surveys/$surveyId/results')
+  );
+}
+
+LocalLoadSurveyResult makeLocalLoadSurveyResult(String surveyId) {
+  return LocalLoadSurveyResult(
+    cacheStorage: makeLocalStorageAdapter()
+  );
+}
+
+LoadSurveyResult makeRemoteLoadSurveyResultWithLocalFallback(String surveyId) {
+  return RemoteLoadSurveyResultWithLocalFallback(
+    remote: makeRemoteLoadSurveyResult(surveyId),
+    local: makeLocalLoadSurveyResult(surveyId)
   );
 }
