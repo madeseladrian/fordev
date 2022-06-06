69e7d44d6f3e34f30cf8d4d902b10ed0a30d0d96
mades eladrian <mades.eladrian@gmail.com>
2022-06-06 11:17:41 -0300

    refact GetxSplashPresenter test

--- test/presentation/presenters/getx_splash_presenter_test.dart
+++ test/presentation/presenters/getx_splash_presenter_test.dart
@@ -1,35 +1,21 @@
-import 'package:faker/faker.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
-import 'package:fordev/domain/entities/entities.dart';
-import 'package:fordev/domain/usecases/usecases.dart';
-
 import 'package:fordev/presentation/presenters/presenters.dart';
-
-class LoadCurrentAccountSpy extends Mock implements LoadCurrentAccount {}
+import '../../domain/mocks/mocks.dart';
 
 void main() {
   late GetxSplashPresenter sut;
   late LoadCurrentAccountSpy loadCurrentAccount;
-  late AccountEntity account;
-
-  When mockLoadCall() => when(() => loadCurrentAccount.load());
-  void mockLoad({required AccountEntity account}) => 
-    mockLoadCall().thenAnswer((_) async => account);
-  void mockLoadError() => mockLoadCall().thenThrow(Exception());
-
-  AccountEntity makeAccount() => AccountEntity(token: faker.guid.guid());
 
   setUp(() {
-    account = makeAccount();
     loadCurrentAccount = LoadCurrentAccountSpy();
-    mockLoad(account: account);
+    loadCurrentAccount.mockLoad(account: EntityFactory.makeAccount());
     sut = GetxSplashPresenter(loadCurrentAccount: loadCurrentAccount);
   });
 
   setUpAll(() {
-    makeAccount();
+    registerFallbackValue(EntityFactory.makeAccount());
   });
  
   test('1 - Should call LoadCurrentAccount', () async {
@@ -44,7 +30,7 @@ void main() {
   });
 
    test('3,4 - Should go to login page on error', () async {
-    mockLoadError();
+    loadCurrentAccount.mockLoadError();
   
     sut.navigateToStream.listen(expectAsync1((page) => expect(page, '/login')));
 
