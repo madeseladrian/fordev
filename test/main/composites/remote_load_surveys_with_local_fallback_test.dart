6d604a0913268839d54c1889b6f5a8795ad7a294
mades eladrian <mades.eladrian@gmail.com>
2022-06-06 10:59:52 -0300

    refact RemoteLoadSurveysWithLocalFallback test

--- test/main/composites/remote_load_surveys_with_local_fallback_test.dart
+++ test/main/composites/remote_load_surveys_with_local_fallback_test.dart
@@ -1,64 +1,30 @@
-import 'package:faker/faker.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
 import 'package:fordev/domain/entities/entities.dart';
 import 'package:fordev/domain/helpers/helpers.dart';
 
-import 'package:fordev/data/usecases/usecases.dart';
 import 'package:fordev/main/composites/composites.dart';
-
-class LocalLoadSurveysSpy extends Mock implements LocalLoadSurveys {}
-class RemoteLoadSurveysSpy extends Mock implements RemoteLoadSurveys {}
+import '../../data/mocks/mocks.dart';
+import '../../domain/mocks/mocks.dart';
 
 void main() {
   late RemoteLoadSurveysWithLocalFallback sut;
-  late LocalLoadSurveysSpy local;
   late RemoteLoadSurveysSpy remote;
+  late LocalLoadSurveysSpy local;
   late List<SurveyEntity> remoteSurveys;
   late List<SurveyEntity> localSurveys;
 
-  When mockLocalSaveCall() => when(() => local.save(any()));
-  void mockLocalSave() => mockLocalSaveCall().thenAnswer((_) async => _);
-
-  When mockLocalValidateCall() => when(() => local.validate());
-  void mockLocalValidate() => mockLocalValidateCall().thenAnswer((_) async => _);
-
-  When mockLocalLoadCall() => when(() => local.load());
-  void mockLocalLoad(List<SurveyEntity> surveys) => mockLocalLoadCall().thenAnswer((_) async => surveys);
-  void mockLocalLoadError() => mockLocalLoadCall().thenThrow(DomainError.unexpected);
-
-  When mockRemoteLoadCall() => when(() => remote.load());
-  void mockRemoteLoad(List<SurveyEntity> surveys) => mockRemoteLoadCall().thenAnswer((_) async => surveys);
-  void mockRemoteLoadError(DomainError error) => mockRemoteLoadCall().thenThrow(error);
-
-  List<SurveyEntity> makeSurveyList() => [
-    SurveyEntity(
-      id: faker.guid.guid(),
-      question: faker.randomGenerator.string(10),
-      dateTime: DateTime.utc(2020, 2, 2),
-      didAnswer: true
-    ),
-    SurveyEntity(
-      id: faker.guid.guid(),
-      question: faker.randomGenerator.string(10),
-      dateTime: DateTime.utc(2018, 12, 20),
-      didAnswer: false
-    )
-  ];
-
   setUp(() {
-    localSurveys = makeSurveyList();
+    localSurveys = EntityFactory.makeSurveyList();
     local = LocalLoadSurveysSpy();
-    mockLocalSave();
-    mockLocalValidate();
-    mockLocalLoad(localSurveys);
-    remoteSurveys = makeSurveyList();
+    local.mockLoad(localSurveys);
+    remoteSurveys = EntityFactory.makeSurveyList();
     remote = RemoteLoadSurveysSpy();
-    mockRemoteLoad(remoteSurveys);
+    remote.mockLoad(remoteSurveys);
     sut = RemoteLoadSurveysWithLocalFallback(
-      local: local,
-      remote: remote
+      remote: remote,
+      local: local
     );
   });
 
@@ -81,7 +47,7 @@ void main() {
   });
 
   test('4 - Should rethrow if remote load throws AccessDeniedError', () async {
-    mockRemoteLoadError(DomainError.accessDenied);
+    remote.mockLoadError(DomainError.accessDenied);
 
     final future = sut.load();
 
@@ -89,7 +55,7 @@ void main() {
   });
 
   test('5,6 - Should call local load on remote error', () async {
-    mockRemoteLoadError(DomainError.unexpected);
+    remote.mockLoadError(DomainError.unexpected);
 
     await sut.load();
 
@@ -98,7 +64,7 @@ void main() {
   });
 
   test('7 - Should return local surveys', () async {
-    mockRemoteLoadError(DomainError.unexpected);
+    remote.mockLoadError(DomainError.unexpected);
 
     final surveys = await sut.load();
 
@@ -106,8 +72,8 @@ void main() {
   });
 
   test('8 - Should throw UnexpectedError if remote and local throws', () async {
-    mockRemoteLoadError(DomainError.unexpected);
-    mockLocalLoadError();
+    remote.mockLoadError(DomainError.unexpected);
+    local.mockLoadError();
 
     final future = sut.load();
 
